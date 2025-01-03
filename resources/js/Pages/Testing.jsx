7f0ff3c7eafb3455ee234f4d371649ab225caeda
Alfi Zain <116138256+AlfiZain@users.noreply.github.com>
2025-01-03 14:13:08 +0700

    persistent layout

--- resources/js/Pages/Testing.jsx
+++ resources/js/Pages/Testing.jsx
@@ -1,5 +1,7 @@
 import AppLayout from '@/Layouts/AppLayout';
 
 export default function Testing() {
-    return <AppLayout>This is testing. Lorem ipsum dolor sit amet. </AppLayout>;
+    return <div>This is testing. Lorem ipsum dolor sit amet.</div>;
 }
+
+Testing.layout = (page) => <AppLayout title="Testing" children={page} />;
